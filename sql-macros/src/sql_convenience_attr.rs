77293270feee4bbcfc8942852b17283da7bf1d3c
LimitLost <46265927+LimitLost@users.noreply.github.com>
2025-04-11 15:59:08 +0200

    Easy Sql: #[sql_convenience] attribute complete!

--- sql-macros/src/sql_convenience_attr.rs
+++ sql-macros/src/sql_convenience_attr.rs
@@ -1,10 +1,12 @@
 use easy_macros::{
-    macros::{all_syntax_cases, always_context, macro_result},
+    anyhow,
+    helpers::parse_macro_input,
+    macros::{all_syntax_cases, always_context},
     proc_macro2::TokenStream,
-    quote::ToTokens,
-    syn,
+    quote::{ToTokens, quote},
+    syn::{self},
 };
-
+#[derive(Debug, Default)]
 struct SqlData {
     potential_table: Option<TokenStream>,
 }
@@ -12,37 +14,92 @@ struct SqlData {
 all_syntax_cases! {
     setup=>{
         generated_fn_prefix:"macro_search",
-        additional_input_type:&mut SearchData
+        additional_input_type:&mut SqlData
     }
     default_cases=>{
-        fn macro_check(item: &mut syn::Macro, context_info: &mut SearchData);
+        fn macro_check(item: &mut syn::Macro, context_info: &mut SqlData);
+        fn call_check(item: &mut syn::ExprCall, context_info: &mut SqlData);
+        fn after_call_check(item: &mut syn::ExprCall, context_info: &mut SqlData);
     }
     special_cases=>{}
 }
 
-fn call_check(item: &mut syn::ExprCall, context_info: &mut SearchData) {
-    item.func
+fn table_function_check(fn_path: &syn::Path) -> Option<TokenStream> {
+    let last_segment = if let Some(s) = fn_path.segments.last() {
+        s
+    } else {
+        return None;
+    };
+    let last_segment_str = last_segment.ident.to_string();
+
+    match last_segment_str.as_str() {
+        "select"
+        | "get"
+        | "select_lazy"
+        | "get_lazy"
+        | "insert"
+        | "insert_returning"
+        | "insert_returning_lazy"
+        | "update"
+        | "update_returning"
+        | "update_returning_lazy"
+        | "delete"
+        | "delete_returning"
+        | "delete_returning_lazy" => {
+            let mut path = fn_path.clone();
+            path.segments.pop();
+            Some(path.to_token_stream())
+        }
+        _ => None,
+    }
 }
 
-fn after_call_check(item: &mut syn::ExprCall, context_info: &mut SearchData) {
-    item.func
-}
-
-fn macro_check(item: &mut syn::Macro, context_info: &mut SearchData) {
-    let path = item.path.to_token_stream().to_string();
-    match path.as_str() {
-        "sql" | "sql_where" | "easy_lib::sql::sql" | "easy_lib::sql::sql_where" => {
-            context_info.found = true;
+fn call_check(item: &mut syn::ExprCall, context_info: &mut SqlData) {
+    match &*item.func {
+        syn::Expr::Path(expr_path) => {
+            context_info.potential_table = table_function_check(&expr_path.path);
         }
         _ => {}
     }
+}
 
-    item
+fn after_call_check(item: &mut syn::ExprCall, context_info: &mut SqlData) {
+    match &*item.func {
+        syn::Expr::Path(_) => {
+            context_info.potential_table = None;
+        }
+        _ => {}
+    }
+}
+
+fn macro_check(item: &mut syn::Macro, context_info: &mut SqlData) {
+    let path = item.path.to_token_stream().to_string();
+    match path.as_str() {
+        "sql" | "sql_where" | "easy_lib::sql::sql" | "easy_lib::sql::sql_where" => {
+            if let Some(table) = &context_info.potential_table {
+                if !table.to_string().starts_with("|") {
+                    replace_with::replace_with_or_abort(&mut item.tokens, |current_tokens| {
+                        quote! {
+                            | #table | #current_tokens
+                        }
+                    });
+                }
+            }
+        }
+        _ => {}
+    }
 }
 
 #[always_context]
 pub fn sql_convenience(
-    attr: proc_macro::TokenStream,
+    _attr: proc_macro::TokenStream,
     item: proc_macro::TokenStream,
 ) -> anyhow::Result<proc_macro::TokenStream> {
+    let mut item = parse_macro_input!(item as syn::Item);
+
+    let mut additional = Default::default();
+
+    macro_search_item_handle(&mut item, &mut additional);
+
+    Ok(item.into_token_stream().into())
 }
