2174e1d7d78e6ed004da7854f0680e4e7fa1026f
LimitLost <46265927+LimitLost@users.noreply.github.com>
2025-05-25 13:01:33 +0200

    Easy SQL: Split between build and not_build features

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,50 @@
 #![doc = include_str!("../README.md")]
 
+#[cfg(feature = "not_build")]
 mod database_structs;
-pub use database_structs::*;
+#[cfg(feature = "not_build")]
 mod easy_executor;
-pub use easy_executor::*;
+#[cfg(feature = "not_build")]
 pub mod never;
+#[cfg(feature = "not_build")]
 mod sql_query;
-use easy_macros::macros::always_context;
-use serde::de::DeserializeOwned;
-pub use sql_query::*;
+#[cfg(feature = "not_build")]
 mod traits;
-pub use traits::*;
 
+#[cfg(feature = "not_build")]
+pub use {
+    database_structs::*, easy_executor::*, sql_compilation_data::SqlType, sql_macros::*,
+    sql_query::*, sqlx::Row as SqlxRow, traits::*,
+};
+
+#[cfg(feature = "not_build")]
+use {easy_macros::macros::always_context, serde::de::DeserializeOwned};
+
+#[cfg(feature = "not_build")]
 type Db = sqlx::Sqlite;
+#[cfg(feature = "not_build")]
 type ConnectionInternal = sqlx::pool::PoolConnection<Db>;
+#[cfg(feature = "not_build")]
 pub type Row = sqlx::sqlite::SqliteRow;
-
+#[cfg(feature = "not_build")]
 pub(crate) type RawConnection = sqlx::SqliteConnection;
 
-pub use sqlx::Row as SqlxRow;
-
-pub use sql_compilation_data::SqlType;
-
-pub use sql_macros::*;
+#[cfg(test)]
+#[cfg(feature = "not_build")]
+use lazy_static::lazy_static;
 
 //Used by SqlTable derive macro (default attribute)
 // pub use lazy_static::lazy_static;
 
+#[cfg(feature = "build")]
+pub use sql_build::*;
+
 #[cfg(test)]
+#[cfg(feature = "not_build")]
 mod tests;
 
 #[always_context]
+#[cfg(feature = "not_build")]
 pub fn from_binary<T: DeserializeOwned>(slice: &[u8]) -> anyhow::Result<T> {
     #[no_context]
     let (result, _) = bincode::serde::decode_from_slice(slice, bincode::config::standard())?;
@@ -39,6 +53,7 @@ pub fn from_binary<T: DeserializeOwned>(slice: &[u8]) -> anyhow::Result<T> {
 }
 
 #[always_context]
+#[cfg(feature = "not_build")]
 pub fn to_binary<T: serde::Serialize>(value: T) -> anyhow::Result<Vec<u8>> {
     #[no_context]
     let result = bincode::serde::encode_to_vec(value, bincode::config::standard())?;
