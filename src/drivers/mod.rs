fb4a0782e7e2aa357bc1b3e4812c10f54ca20f08
LimitLost <46265927+LimitLost@users.noreply.github.com>
2025-10-19 16:36:37 +0200

    Hide drivers behind features

--- src/drivers/mod.rs
+++ src/drivers/mod.rs
@@ -1,6 +1,10 @@
+#[cfg(feature = "postgres")]
 pub mod postgres;
+#[cfg(feature = "sqlite")]
 pub mod sqlite;
 
 pub mod general_value;
-
+#[cfg(feature = "postgres")]
+pub use postgres::Postgres;
+#[cfg(feature = "sqlite")]
 pub use sqlite::Sqlite;
