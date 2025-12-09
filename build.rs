a971ad82176f3d38ead0afebf5bc85d0d4441763
LimitLost <46265927+LimitLost@users.noreply.github.com>
2025-12-09 20:40:45 +0100

    Easy Macros: Always Context Update (ignore macros)

--- build.rs
+++ build.rs
@@ -1,5 +1,10 @@
+use easy_macros::always_context_build::{BuildOptions, Skip};
+
 fn main() {
-    easy_macros::always_context_build::build(&[regex::Regex::new(r"readme\.rs").unwrap()]);
+    easy_macros::always_context_build::build_with_options(
+        &[regex::Regex::new(r"readme\.rs").unwrap()],
+        BuildOptions::new().skip(vec![Skip::MacrosShort]),
+    );
     sql_build::build(
         &[regex::Regex::new(r"example_all\.rs").unwrap()],
         #[cfg(not(any(feature = "sqlite", feature = "postgres")))]
